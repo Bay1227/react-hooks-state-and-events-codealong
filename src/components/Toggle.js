fef27fd19bf668a4bbefc217de36b89941c1ea38
Bayartsengel Battulga <tsengelbattulga1227@gmail.com>
2021-08-13 00:32:23 -0700

    commit tree fd9942d7a27026fc6926f46b4fb5e031c8156cdd

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,25 @@
-import React from "react";
+// import React from "react";
+import React, { useState } from "react";
 
-function Toggle() {
-  return <button>OFF</button>;
+// function Toggle() {
+//   const [isOn, setIsOn] = useState(false);
+//   // return <button>OFF</button>;
+//   <button>{isOn ? "ON" : "OFF"}</button>;
+// }
+
+// export default Toggle;
+export default function Toggle() {
+  const [isOn, setIsOn] = useState(false);
+
+  function handleClick() {
+    setIsOn((isOn) => !isOn);
+  }
+
+  const color = isOn ? "red" : "white";
+
+  return (
+    <button style={{ background: color }} onClick={handleClick}>
+      {isOn ? "ON" : "OFF"}
+    </button>
+  );
 }
-
-export default Toggle;
